a3400cdbaf71208a61f5359dfd6a16fc4c8ee091
secondarykey <secondarykey@gmail.com>
2015-11-07 10:44:34 +0900

    Add Method-17

--- 20151121/secondarykey/sort.go
+++ 20151121/secondarykey/sort.go
@@ -5,13 +5,13 @@ import (
 	"sort"
 )
 
-type Hoge []int
-
 func main() {
 	hoge := Hoge{1, 4, 2, 0, 6, 9, 3, 5, 8, 7}
 	sort.Sort(hoge)
 	fmt.Println(hoge)
 }
+
+type Hoge []int
 
 func (h Hoge) Len() int {
 	return len(h)
